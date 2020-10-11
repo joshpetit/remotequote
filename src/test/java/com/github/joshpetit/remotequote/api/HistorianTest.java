043d2d1b873f062bf388570223d3fe1acdabd29b
joshu <joshuapetitma@yahoo.com>
2020-10-11 11:48:19 -0400

    udpated getAllUserQuotes test

--- src/test/java/com/github/joshpetit/remotequote/api/HistorianTest.java
+++ src/test/java/com/github/joshpetit/remotequote/api/HistorianTest.java
@@ -7,16 +7,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class HistorianTest {
-    static Historian historian;
 
     @BeforeAll
     static void setup() {
-        historian = new Historian("test_user1");
+        Historian.initializeHistorian("test_user1");
     }
 
     @Test
     void getAllQuotes() {
-        Quote q = historian.getAllUserQuotes().get(0);
+        Quote q = Historian.getAllUserQuotes().get(0);
         assert (q != null);
     }
 
