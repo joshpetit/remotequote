4dee414680e72c52450d6febed046f21b62ebca0
joshu <joshuapetitma@yahoo.com>
2020-10-11 11:13:33 -0400

    finished implementing getAllUserQuotes

--- src/main/java/com/github/joshpetit/remotequote/api/Historian.java
+++ src/main/java/com/github/joshpetit/remotequote/api/Historian.java
@@ -24,36 +24,22 @@ public class Historian {
             dbUser = creds.getProperty("username");
             dbPass = creds.getProperty("password");
             session = CqlSession.builder()
-            .withCloudSecureConnectBundle(Historian.class.getResource("secure-connect-central.zip"))
-            .withAuthCredentials(dbUser, dbPass)
-            .withKeyspace("quotes")
-            .build();
+                    .withCloudSecureConnectBundle(Historian.class.getResource("secure-connect-central.zip"))
+                    .withAuthCredentials(dbUser, dbPass)
+                    .withKeyspace("quotes")
+                    .build();
         } catch (IOException e) {
             System.err.println("Unable to connect to database");
             System.exit(1);
         }
     }
 
-
     public List<Quote> getAllUserQuotes() {
         List<Quote> quotes = new ArrayList<>();
-        ResultSet rs = session.execute("select * from user_quotes");
-        rs.forEach(row -> quotes.add(new Quote(row.getString("quote"), row.getString("author"))));
-        return quotes;
-    }
-
-    public List<Quote> getQuotesFrom(String author) {
-        String query = String.format("select * from user_quotes where author = %s ALLOW FILTERING", author);
+        String query = String.format("select * from user_quotes where username='%s'", username);
         ResultSet rs = session.execute(query);
-        return fillQuotes(rs);
-    }
-
-    private List<Quote> fillQuotes(ResultSet rs) {
-        return rs.map(x -> new Quote(x.getString("quote"), x.getString("author"))).all();
-    }
-
-    public void addQuote(String quote, String author) {
-        String query = String.format("INSERT INTO user_quotes (quote, author) VALUES ('%s', '%s')", quote, author);
-        session.execute(query);
+        rs.forEach(row -> quotes.add(new Quote(row.getString("quote"),
+                row.getString("author"), row.getUuid("quote_id"), row.getString("username"))));
+        return quotes;
     }
 }
